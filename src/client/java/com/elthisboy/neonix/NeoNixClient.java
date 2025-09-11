aaa1c97f56a0d9bf99ef2eac9cf7c578a01f5fcf
Elthisboy <cristopher.12c10@gmail.com>
2025-09-11 18:34:37 -0300

    start

---
+++ src/client/java/com/elthisboy/neonix/NeoNixClient.java
@@ -0,0 +1,10 @@
+package com.elthisboy.neonix;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NeoNixClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/elthisboy/neonix/NeoNixDataGenerator.java
@@ -0,0 +1,11 @@
+package com.elthisboy.neonix;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NeoNixDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
